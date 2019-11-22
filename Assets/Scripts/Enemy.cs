81282bcb1ed3ee12e4af9a5d2a5bb58296f16427
Lawrence Dang <lawrend1@uci.edu>
2019-11-21 17:22:17 -0800

    Simple enemy movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth = 1;
+    [HideInInspector]
+    public int Health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
