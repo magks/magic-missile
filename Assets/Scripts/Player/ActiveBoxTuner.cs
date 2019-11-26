22a6941188ef17aa4d7d054d1d911a260ee592b0
Lawrence Dang <lawrend1@uci.edu>
2019-11-25 17:27:12 -0800

    New random delay shooting

---
+++ Assets/Scripts/Player/ActiveBoxTuner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveBoxTuner : MonoBehaviour
+{
+    BoxCollider2D activeArea;
+    Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeArea = GetComponent<BoxCollider2D>();
+        cam = Camera.main;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float halfHeight = cam.orthographicSize;
+        float halfWidth = halfHeight * cam.aspect;
+        activeArea.size = new Vector2(halfWidth * 2, halfHeight *2);
+    }
+}
