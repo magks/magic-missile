98d12be3d2db1d30f690b014547feb663f59c0e3
Lawrence Dang <lawrend1@uci.edu>
2019-12-02 17:14:03 -0800

    started work on enemy sounds

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    static float FadeTime = 1;
     public int maxHealth = 1;
     [HideInInspector]
     public int Health;
+    SpriteRenderer sprite;
     // Start is called before the first frame update
     void Start()
     {
         Health = maxHealth;
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Die()
+    {
+
+    }
+
+    public IEnumerator DeathFade()
+    {
+        float animTime = 0;
+        float startOpacity = sprite.color.a;
+        Color newColor = sprite.color;
+        while(animTime < FadeTime)
+        {
+            newColor.a = startOpacity * ((FadeTime - animTime) / FadeTime);
+            sprite.color = newColor;
+            animTime += Time.deltaTime;
+            yield return null;
+        }
+        
     }
 
     // Update is called once per frame
