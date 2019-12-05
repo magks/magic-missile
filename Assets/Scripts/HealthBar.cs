3730ed6c8a49470d86d06fd18aa25e41b5100340
Lawrence Dang <lawrend1@uci.edu>
2019-12-04 19:24:28 -0800

    Health bar; death anim

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public GameObject playerObj;
+    [SerializeField]
+    GameObject[] barObjects;
+    Player playerStatus;
+    int hp_state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerStatus = playerObj.GetComponent<Player>();
+        hp_state = 3;
+        barObjects[hp_state].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hp_state != playerStatus.Health)
+        {
+            barObjects[hp_state].SetActive(false);
+            hp_state = playerStatus.Health;
+            barObjects[hp_state].SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -25,19 +25,51 @@ public class Player : MonoBehaviour
             }
         }
     }
+    [SerializeField]
+    private GameObject deathScreen;
+    [SerializeField]
+    private GameObject playerSprite;
     private int _curHealth;
-
+    bool dead;
     // Start is called before the first frame update
     void Start()
     {
         _curHealth = maxHP;
+        dead = false;
+    }
+
+    IEnumerator FadeToBlack()
+    {
+        SpriteRenderer screen = deathScreen.GetComponent<SpriteRenderer>();
+        SpriteRenderer sprite = playerSprite.GetComponent<SpriteRenderer>();
+        Color scrnColor = screen.color;
+        Color playColor = sprite.color;
+        float opacity = 0;
+        float fadeSeconds = 4;
+        float timeLeft = 0;
+        while (opacity != 1)
+        {
+            yield return null;
+            timeLeft += Time.deltaTime;
+            opacity = Mathf.Min(1, timeLeft / fadeSeconds);
+            scrnColor.a = opacity;
+            playColor.a = 1 - opacity;
+            screen.color = scrnColor;
+            sprite.color = playColor;
+        }
+        SceneManager.LoadScene("DeathScreen");
     }
 
     void Die()
     {
         //SceneManager.LoadScene()
         Camera.main.transform.SetParent(null, true);
-        Destroy(gameObject);
+        dead = true;
+        GetComponent<PlayerMovement>().enabled = false;
+        GetComponent<Shoot>().enabled = false;
+        GetComponent<Rigidbody2D>().simulated = false;
+        StartCoroutine(FadeToBlack());
+        deathScreen.transform.SetParent(null, true);
     }
 
     public void TakeDamage(int amount)
@@ -48,8 +80,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(_curHealth);
-       if(_curHealth==0)
+       if(_curHealth==0 && !dead)
         {
             Die();
         }
